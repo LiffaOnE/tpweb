6e63385d41c5b9327077253b181fa301a77a0541
LiffaOnE <feliz.fuentes67@gmail.com>
2021-10-18 17:23:00 -0300

    navbar

--- pw-tpweb/src/App.js
+++ pw-tpweb/src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+function openNav() {
+  document.getElementById("mobile__menu").style.width = "100%"
+}
+function closeNav() {
+  document.getElementById("mobile__menu").style.width = "0"
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+  <div>
+    <noscript>You need to enable JavaScript to run this app.</noscript>
+    <div id="root"></div>
+
+      This HTML file is a template.
+      If you open it directly in the browser, you will see an empty page.
+
+      You can add webfonts, meta tags, or analytics to this file.
+      The build step will place the bundled scripts into the <body> tag.
+
+      To begin the development, run `npm start` or `yarn start`.
+      To create a production bundle, use `npm run build` or `yarn build`.
+  </div>
+  </body>
   );
 }
 
--- pw-tpweb/src/setupTests.js
+++ pw-tpweb/src/setupTests.js
@@ -3,3 +3,10 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+function resolveAfter2Seconds
+async function asyncCall() {
+    const result = resolveAfter2Seconds();
+    console.log(result)
+    // expected output: "resolved"
+    }
+    asyncCall();
