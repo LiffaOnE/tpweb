ae06f3eef6659fdf6ddbc1be1ee95f318598c561
czabcic <czabcic@mail.austral.edu.ar>
2021-11-02 16:20:30 -0300

    ruta register y ruta login (videos 29 y 30)

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from api.views import ServiceViewSet
+from api.views import ServiceViewSet, RegisterView
 
 router = routers.DefaultRouter()
 router.register(r'services', ServiceViewSet)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register', RegisterView.as_view())
 ]
+
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 
 from api.models import Service
-from api.serializers import ServiceSerializer
+from api.serializers import ServiceSerializer, RegisterSerializer
+
 
 class ServiceViewSet(viewsets.ModelViewSet):
     serializer_class = ServiceSerializer
     queryset = Service.objects.all()
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
