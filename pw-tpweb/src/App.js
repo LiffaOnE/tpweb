33734a41a8d946638ed609fc643eb3525d542940
LiffaOnE <feliz.fuentes67@gmail.com>
2021-11-13 14:41:04 -0300

    yacasi

--- pw-tpweb/src/App.js
+++ pw-tpweb/src/App.js
@@ -1,31 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
 function openNav() {
-  document.getElementById("mobile__menu").style.width = "100%"
+  document.getElementById("mobile__menu").style.width = "100%";
 }
 function closeNav() {
-  document.getElementById("mobile__menu").style.width = "0"
+  document.getElementById("mobile__menu").style.width = "0";
 }
-
-function App() {
-  return (
-    <body>
-  <div>
-    <noscript>You need to enable JavaScript to run this app.</noscript>
-    <div id="root"></div>
-
-      This HTML file is a template.
-      If you open it directly in the browser, you will see an empty page.
-
-      You can add webfonts, meta tags, or analytics to this file.
-      The build step will place the bundled scripts into the <body> tag.
-
-      To begin the development, run `npm start` or `yarn start`.
-      To create a production bundle, use `npm run build` or `yarn build`.
-  </div>
-  </body>
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel(
+  {
+      nav:true,
+      margin:10,
+      dots:false,
+      Responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:3
+          },
+          1000:{
+              items:5,
+          }
+      }
+  }
   );
-}
-
-export default App;
+});
